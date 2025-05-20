25fada9196b37db126eb9ecfc524f059bd354db5
emil lenz <emillenz@protonmail.com>
2025-05-20 12:54:39 +0200

    emacs :: using ~setopt~ (more typechecking)

--- emacs/init.el
+++ emacs/init.el
@@ -6,8 +6,8 @@
 ;; email: emillenz@protonmail.com
 ;; ---
 
-(setq use-package-always-demand t
-      use-package-enable-imenu-support t)
+(setopt use-package-always-demand t
+	use-package-enable-imenu-support t)
 
 (use-package emacs
   :config
@@ -33,75 +33,75 @@
   (horizontal-scroll-bar-mode -1)
   (tooltip-mode -1)
 
-  (setq history-length 1000
-	history-delete-duplicates t
-	uniquify-buffer-name-style 'forward
-	delete-by-moving-to-trash t
-	remote-file-name-inhibit-delete-by-moving-to-trash t
-	ring-bell-function 'ignore
-	enable-recursive-minibuffers t
-	global-auto-revert-non-file-buffers t
-	auto-save-include-big-deletions t
-	kill-buffer-delete-auto-save-files t
-	auto-save-list-file-prefix (expand-file-name "autosave/" user-emacs-directory)
-	use-short-answers t
-	save-interprogram-paste-before-kill t
-	require-final-newline t
-	load-prefer-newer t
-	shell-command-prompt-show-cwd t
-	async-shell-command-display-buffer nil
-	async-shell-command-buffer 'new-buffer
-	custom-file (expand-file-name "custom.el" user-emacs-directory)
-	desktop-dirname user-emacs-directory
-	find-file-visit-truename t
-	comment-empty-lines nil
-	register-preview-delay nil
-	kill-do-not-save-duplicates t
-	show-paren-when-point-inside-paren t
-	kill-whole-line t
-	shift-select-mode nil
-	set-mark-command-repeat-pop t
-	bookmark-fringe-mark nil
+  (setopt history-length 1000
+	  history-delete-duplicates t
+	  uniquify-buffer-name-style 'forward
+	  delete-by-moving-to-trash t
+	  remote-file-name-inhibit-delete-by-moving-to-trash t
+	  ring-bell-function 'ignore
+	  enable-recursive-minibuffers t
+	  global-auto-revert-non-file-buffers t
+	  auto-save-include-big-deletions t
+	  kill-buffer-delete-auto-save-files t
+	  auto-save-list-file-prefix (expand-file-name "autosave/" user-emacs-directory)
+	  use-short-answers t
+	  save-interprogram-paste-before-kill t
+	  require-final-newline t
+	  load-prefer-newer t
+	  shell-command-prompt-show-cwd t
+	  async-shell-command-display-buffer nil
+	  async-shell-command-buffer 'new-buffer
+	  custom-file (expand-file-name "custom.el" user-emacs-directory)
+	  desktop-dirname user-emacs-directory
+	  find-file-visit-truename t
+	  comment-empty-lines nil
+	  register-preview-delay nil
+	  kill-do-not-save-duplicates t
+	  show-paren-when-point-inside-paren t
+	  kill-whole-line t
+	  shift-select-mode nil
+	  set-mark-command-repeat-pop t
+	  bookmark-fringe-mark nil)
 
-	create-lockfiles nil
-	make-backup-files nil
-	delete-old-versions t
-	version-control t
-	kept-new-versions 5
-	kept-old-versions 5
-	vc-follow-symlinks t
-	vc-make-backup-files nil
-	backup-by-copying t
-	backup-by-copying-when-linked t
-	backup-directory-alist (list (cons "." (concat user-emacs-directory "backups")))
+  (setopt create-lockfiles nil
+	  make-backup-files nil
+	  delete-old-versions t
+	  version-control t
+	  kept-new-versions 5
+	  kept-old-versions 5
+	  vc-follow-symlinks t
+	  vc-make-backup-files nil
+	  backup-by-copying t
+	  backup-by-copying-when-linked t
+	  backup-directory-alist (list (cons "." (concat user-emacs-directory "backups"))))
 
-	initial-scratch-message nil
-	inhibit-startup-echo-area-message user-login-name
-	initial-buffer-choice nil
-	inhibit-splash-screen t
-	inhibit-startup-screen t
-	inhibit-startup-buffer-menu t
-	frame-title-format "%b ― emacs"
-	icon-title-format frame-title-format
-	frame-inhibit-implied-resize t
-	use-file-dialog nil
-	use-dialog-box nil
+  (setopt initial-scratch-message nil
+	  inhibit-startup-echo-area-message user-login-name
+	  initial-buffer-choice nil
+	  inhibit-splash-screen t
+	  inhibit-startup-screen t
+	  inhibit-startup-buffer-menu t
+	  frame-title-format "%b ― emacs"
+	  icon-title-format frame-title-format
+	  frame-inhibit-implied-resize t
+	  use-file-dialog nil
+	  use-dialog-box nil)
 
-	tab-always-indent 'complete
-	indent-tabs-mode t
-	tab-width 8
-	standard-indent 8
-	c-default-style "linux"
+  (setopt tab-always-indent 'complete
+	  indent-tabs-mode t
+	  tab-width 8
+	  standard-indent 8
+	  c-default-style "linux")
 
-	compilation-scroll-output t
-	next-error-recenter '(4)
+  (setopt compilation-scroll-output t
+	  next-error-recenter '(4))
 
-	scroll-preserve-screen-position t
-	scroll-error-top-bottom t
-	auto-window-vscroll nil
+  (setopt scroll-preserve-screen-position t
+	  scroll-error-top-bottom t
+	  auto-window-vscroll nil)
 
-	comint-input-ignoredups t
-	comint-prompt-read-only t)
+  (setopt comint-input-ignoredups t
+	  comint-prompt-read-only t)
 
   (progn
     (global-hl-line-mode)
@@ -115,20 +115,20 @@
   (add-to-list 'default-frame-alist '(font . "iosevka comfy-10"))
 
   (progn
-    (setq minibuffer-prompt-properties
-	  '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
+    (setopt minibuffer-prompt-properties
+	    '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
     (add-hook 'minibuffer-setup-hook 'cursor-intangible-mode))
 
   (progn
     (set-language-environment "utf-8")
-    (setq default-input-method nil))
+    (setopt default-input-method nil))
 
   (progn
     (require-theme 'modus-themes)
-    (setq modus-themes-italic-constructs t
-	  modus-themes-bold-constructs t
-	  modus-themes-common-palette-overrides '((fg-heading-1 fg-heading-0)
-						  (bg-prose-block-contents bg-dim)))
+    (setopt modus-themes-italic-constructs t
+	    modus-themes-bold-constructs t
+	    modus-themes-common-palette-overrides '((fg-heading-1 fg-heading-0)
+						    (bg-prose-block-contents bg-dim)))
     (load-theme 'modus-operandi)
 
     (progn
@@ -158,133 +158,151 @@
 				(when (mark) (= (mark) (point))))
 		      (push-mark)))))
 
-    (seq-do 'push-mark-once '(mark-paragraph backward-up-list)))
+    (seq-do 'push-mark-once '(mark-paragraph
+			      backward-up-list)))
 
   (seq-do (lambda (cmd)
 	    (advice-add cmd :around
 			(lambda (fn &rest args)
 			  (with-undo-amalgamate
 			    (apply fn args)))))
-	  '(kmacro-end-or-call-macro-repeat
+	  '(kmacro-end-and-call-macro
+	    kmacro-end-or-call-macro-repeat
 	    query-replace-regexp
 	    apply-macro-to-region-lines))
 
   (progn
-    (defun kill-ring-save-region-or-next-kill ()
-      (interactive)
-      (if (region-active-p)
-	  (call-interactively 'kill-ring-save)
-	(let ((buffer-read-only t))
-	  (ignore-errors
-	    (save-mark-and-excursion
-	      (call-interactively
-	       (key-binding
-		(read-key-sequence "save next kill:"))))))))
+    (defmacro keymap-set! (keymap &rest pairs)
+      (macroexp-progn
+       (cl-loop for (key cmd)
+		on pairs
+		by 'cddr
+		collect (list 'keymap-set keymap key cmd))))
 
-    (defun open-line-indent ()
-      (interactive)
-      (save-mark-and-excursion
-	(seq-do 'call-interactively '(open-line forward-line))
-	(indent-according-to-mode)))
+    (keymap-set! global-map
+		 "<remap> <downcase-word>" 'downcase-dwim
+		 "<remap> <upcase-word>" 'upcase-dwim
+		 "<remap> <capitalize-word>" 'capitalize-dwim
+		 "<remap> <dabbrev-expand>" 'hippie-expand
+		 "<remap> <kill-buffer>" 'kill-buffer-and-window
+		 "<remap> <list-buffers>" 'ibuffer
 
-    (defun indent-dwim ()
-      (interactive)
-      (call-interactively
-       (if (region-active-p)
-	   'indent-region
-	 'indent-for-tab-command)))
+		 "<remap> <delete-horizontal-space>" 'cycle-spacing
+		 "M-SPC" 'mark-word
 
-    (defun yank-indent ()
-      (interactive)
-      (let ((pos (point)))
-	(if (and delete-selection-mode (region-active-p))
-	    (call-interactively 'delete-region))
-	(call-interactively 'yank)
-	(indent-region pos (point))))
+		 "C-u" (lambda () (interactive) (set-mark-command 1))
+		 "C-z" 'repeat)
 
-    (defun comment-sexp-dwim ()
-      (interactive)
-      (save-mark-and-excursion
-	(unless (region-active-p)
-	  (call-interactively 'mark-sexp))
-	(call-interactively 'comment-dwim)))
+    (keymap-set! ctl-x-map
+		 "f" 'recentf-open)
 
-    (defun eval-sexp-dwim ()
-      (interactive)
-      (save-mark-and-excursion
-	(unless (region-active-p)
-	  (call-interactively 'mark-sexp))
-	(eval-region (region-beginning) (region-end) t)))
+    (keymap-set! ctl-x-x-map
+		 "f" 'global-font-lock-mode)
 
-    (defun pp-eval-sexp-dwim ()
-      (interactive)
-      (save-mark-and-excursion
-	(unless (region-active-p)
-	  (call-interactively 'mark-sexp))
-	(pp-eval-expression (read (buffer-substring (region-beginning)
-						    (region-end))))))
+    (keymap-set! indent-rigidly-map
+		 "C-i" 'indent-rigidly-right-to-tab-stop
+		 "C-M-i" 'indent-rigidly-left-to-tab-stop
+		 "SPC" 'indent-rigidly-right
+		 "DEL" 'indent-rigidly-left)
 
-    (defun switch-to-other-buffer ()
-      (interactive)
-      (let ((buf (caar (window-prev-buffers))))
-	(switch-to-buffer (unless (eq buf (current-buffer)) buf))))
-
-    (defun buffer-to-register (reg)
-      (interactive (list (register-read-with-preview "buffer to register:")))
-      (set-register reg (cons 'buffer (current-buffer))))
-
-    (defun point-to-register-dwim ()
-      (interactive)
-      (call-interactively
-       (if current-prefix-arg
-	   'buffer-to-register
-	 'point-to-register)))
+    (keymap-set! next-error-repeat-map
+		 "<" 'first-error)
 
     (progn
-      (defmacro keymap-set! (keymap &rest pairs)
-	(macroexp-progn
-	 (cl-loop for (key cmd)
-		  on pairs
-		  by 'cddr
-		  collect (list 'keymap-set keymap key cmd))))
+      (defun kill-ring-save-region-or-next-kill ()
+	(interactive)
+	(if (region-active-p)
+	    (call-interactively 'kill-ring-save)
+	  (let ((buffer-read-only t))
+	    (ignore-errors
+	      (save-mark-and-excursion
+		(call-interactively
+		 (key-binding
+		  (read-key-sequence "save next kill:"))))))))
+
+      (keymap-set! global-map "M-w" 'kill-ring-save-region-or-next-kill))
+
+    (progn
+      (defun open-line-indent ()
+	(interactive)
+	(save-mark-and-excursion
+	  (seq-do 'call-interactively '(open-line forward-line))
+	  (indent-according-to-mode)))
+
+      (keymap-set! global-map "<remap> <open-line>" 'open-line-indent))
+
+    (progn
+      (defun indent-dwim ()
+	(interactive)
+	(call-interactively
+	 (if (region-active-p)
+	     'indent-region
+	   'indent-for-tab-command)))
+
+      (keymap-set! global-map "<remap> <indent-for-tab-command>" 'indent-dwim))
+
+    (progn
+      (defun yank-indent ()
+	(interactive)
+	(let ((pos (point)))
+	  (if (and delete-selection-mode (region-active-p))
+	      (call-interactively 'delete-region))
+	  (call-interactively 'yank)
+	  (indent-region pos (point))))
+
+      (keymap-set! global-map "<remap> <yank>" 'yank-indent))
+
+    (progn
+      (defun comment-sexp-dwim ()
+	(interactive)
+	(save-mark-and-excursion
+	  (unless (region-active-p)
+	    (call-interactively 'mark-sexp))
+	  (call-interactively 'comment-dwim)))
+
+      (keymap-set! global-map "<remap> <comment-dwim>" 'comment-sexp-dwim))
+
+    (progn
+      (defun eval-sexp-dwim ()
+	(interactive)
+	(save-mark-and-excursion
+	  (unless (region-active-p)
+	    (call-interactively 'mark-sexp))
+	  (eval-region (region-beginning) (region-end) t)))
+
+      (defun pp-eval-sexp-dwim ()
+	(interactive)
+	(save-mark-and-excursion
+	  (unless (region-active-p)
+	    (call-interactively 'mark-sexp))
+	  (pp-eval-expression (read (buffer-substring (region-beginning)
+						      (region-end))))))
+
+      (keymap-set! global-map "<remap> <eval-last-sexp>" 'eval-sexp-dwim))
+
+    (progn
+      (defun switch-to-other-buffer ()
+	(interactive)
+	(let ((buf (caar (window-prev-buffers))))
+	  (switch-to-buffer (unless (eq buf (current-buffer)) buf))))
+
+      (keymap-set! global-map "M-o" 'switch-to-other-buffer))
+
+    (progn
+      (defun buffer-to-register (reg)
+	(interactive (list (register-read-with-preview "buffer to register:")))
+	(set-register reg (cons 'buffer (current-buffer))))
+
+      (defun point-to-register-dwim ()
+	(interactive)
+	(call-interactively
+	 (if current-prefix-arg
+	     'buffer-to-register
+	   'point-to-register)))
 
       (keymap-set! global-map
-		   "<remap> <downcase-word>" 'downcase-dwim
-		   "<remap> <yank>" 'yank-indent
-		   "<remap> <indent-for-tab-command>" 'indent-dwim
-		   "<remap> <upcase-word>" 'upcase-dwim
-		   "<remap> <capitalize-word>" 'capitalize-dwim
-		   "<remap> <dabbrev-expand>" 'hippie-expand
-		   "<remap> <eval-last-sexp>" 'eval-sexp-dwim
-		   "<remap> <comment-dwim>" 'comment-sexp-dwim
-		   "<remap> <open-line>" 'open-line-indent
-		   "<remap> <kill-buffer>" 'kill-buffer-and-window
-		   "<remap> <list-buffers>" 'ibuffer
-
-		   "<remap> <delete-horizontal-space>" 'cycle-spacing
-		   "M-SPC" 'mark-word
-
-		   "C-u" (lambda () (interactive) (set-mark-command 1))
-		   "C-z" 'repeat
-		   "M-w" 'kill-ring-save-region-or-next-kill
 		   "M-r" 'point-to-register-dwim
-		   "M-j" 'jump-to-register)
-
-      (keymap-set! ctl-x-map
-		   "f" 'recentf-open
-		   "C-b" 'switch-to-other-buffer)
-
-      (keymap-set! ctl-x-x-map
-		   "f" 'global-font-lock-mode)
-
-      (keymap-set! indent-rigidly-map
-		   "C-i" 'indent-rigidly-right-to-tab-stop
-		   "C-M-i" 'indent-rigidly-left-to-tab-stop
-		   "SPC" 'indent-rigidly-right
-		   "DEL" 'indent-rigidly-left)
-
-      (keymap-set! next-error-repeat-map
-		   "<" 'first-error))))
+		   "M-j" 'jump-to-register))))
 
 (use-package isearch
   :config
@@ -295,13 +313,13 @@
 			 (string-prefix-p "isearch" (symbol-name last-command)))
 		(goto-char isearch-other-end))))
 
-  (setq isearch-lax-whitespace t
-	search-whitespace-regexp ".*?"
+  (setopt isearch-lax-whitespace t
+	  search-whitespace-regexp ".*?"
 
-	lazy-highlight-initial-delay 0
-	isearch-lazy-count t
-	isearch-allow-motion t
-	isearch-motion-changes-direction t)
+	  lazy-highlight-initial-delay 0
+	  isearch-lazy-count t
+	  isearch-allow-motion t
+	  isearch-motion-changes-direction t)
 
   (keymap-unset isearch-mode-map "C-w" t)
   (keymap-unset isearch-mode-map "C-M-d" t)
@@ -328,14 +346,14 @@
   :config
   (fido-vertical-mode)
 
-  (setq max-mini-window-height 12
-	completions-detailed t
-	completions-auto-help 'visible
-	completions-max-height 16
-	completions-format 'one-column
-	icomplete-compute-delay 0
-	read-buffer-completion-ignore-case t
-	read-file-name-completion-ignore-case t)
+  (setopt max-mini-window-height 12
+	  completions-detailed t
+	  completions-auto-help 'visible
+	  completions-max-height 16
+	  completions-format 'one-column
+	  icomplete-compute-delay 0
+	  read-buffer-completion-ignore-case t
+	  read-file-name-completion-ignore-case t)
 
   (keymap-set! minibuffer-mode-map
 	       "<remap> <minibuffer-complete>" 'icomplete-force-complete
@@ -344,30 +362,30 @@
 (use-package recentf
   :config
   (recentf-mode 1)
-  (setq recentf-max-saved-items 300))
+  (setopt recentf-max-saved-items 300))
 
 (use-package savehist
   :config
   (savehist-mode 1)
-  (setq savehist-additional-variables
-	'(kill-ring
-	  register-alist
-	  mark-ring global-mark-ring
-	  search-ring regexp-search-ring)))
+  (setopt savehist-additional-variables
+	  '(kill-ring
+	    register-alist
+	    mark-ring global-mark-ring
+	    search-ring regexp-search-ring)))
 
 (use-package dired
   :config
   (add-hook 'dired-mode-hook 'dired-hide-details-mode)
   (add-hook 'dired-mode-hook 'dired-omit-mode)
 
-  (setq dired-free-space nil
-	dired-omit-files "^\\..*$"
-	dired-clean-confirm-killing-deleted-buffers nil
-	dired-recursive-copies 'always
-	dired-recursive-deletes 'always
-	dired-no-confirm '(uncompress move copy)
-	dired-create-destination-dirs 'ask
-	dired-vc-rename-file t)
+  (setopt dired-free-space nil
+	  dired-omit-files "^\\..*$"
+	  dired-clean-confirm-killing-deleted-buffers nil
+	  dired-recursive-copies 'always
+	  dired-recursive-deletes 'always
+	  dired-no-confirm '(uncompress move copy)
+	  dired-create-destination-dirs 'ask
+	  dired-vc-rename-file t)
 
   (keymap-set! dired-mode-map
 	       "<remap> <dired-hide-details-mode>" (lambda ()
@@ -377,7 +395,7 @@
 
 (use-package org
   :config
-  (setq org-tags-column 0))
+  (setopt org-tags-column 0))
 
 (use-package package
   :config
@@ -389,8 +407,8 @@
   :init (puni-global-mode 1)
   :config
   (seq-do 'push-mark-once '(puni-mark-list-around-point
-			  puni-end-of-sexp
-			  puni-beginning-of-sexp))
+			    puni-end-of-sexp
+			    puni-beginning-of-sexp))
 
   (advice-add 'puni-kill-line
 	      :around
@@ -434,7 +452,7 @@
   :ensure t
   :init (current-window-only-mode 1)
   :config
-  (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+  (setopt ediff-window-setup-function 'ediff-setup-windows-plain)
   (advice-remove 'delete-other-windows 'current-window-only--delete-other-windows)
   (add-to-list 'display-buffer-alist (list (rx (seq "*")
 					       (or "Completions"
@@ -446,4 +464,4 @@
   :ensure t
   :init (pdf-tools-install)
   :config
-  (setq pdf-view-display-size 'fit-height))
+  (setopt pdf-view-display-size 'fit-height))
